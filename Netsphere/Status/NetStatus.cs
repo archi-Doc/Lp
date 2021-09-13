6616bc0d0338d0794d6b0a7756b6bca5a117a522
archi-Doc <archidoc422@gmail.com>
2021-09-14 00:18:59 +0900

    .

---
+++ Netsphere/Status/NetStatus.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+using System;
+using Arc.Threading;
+
+namespace LP.Netsphere;
+
+public class NetStatus
+{
+    public NetStatus()
+    {
+    }
+}
