609fdd6eb916ed133d16052cd6921bc5ff7be8a9
archi-Doc <archidoc422@gmail.com>
2021-09-17 17:36:01 +0900

    Add IdentifierBenchmark

---
+++ Benchmark/Benchmarks/IdentifierBenchmark.cs
@@ -0,0 +1,62 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Arc.Crypto;
+using BenchmarkDotNet.Attributes;
+
+namespace Benchmark;
+
+[Config(typeof(BenchmarkConfig))]
+public class IdentifierBenchmark
+{
+    public IdentifierBenchmark()
+    {
+        this.ByteArray = new byte[this.Length];
+        for (var i = 0; i < this.Length; i++)
+        {
+            this.ByteArray[i] = (byte)i;
+        }
+    }
+
+    [Params(10)]
+    public int Length { get; set; }
+
+    public byte[] ByteArray { get; } = default!;
+
+    public SHA3_256 Instance { get; } = new();
+
+    [Benchmark]
+    public Identifier_ClassULong Identifier_ClassULong()
+    {
+        var r = this.Instance.GetHashULong(this.ByteArray);
+        return new Identifier_ClassULong(r.hash0, r.hash1, r.hash2, r.hash3);
+    }
+
+    [Benchmark]
+    public Identifier_ClassByte Identifier_ClassByte()
+    {
+        var r = this.Instance.GetHash(this.ByteArray);
+        return new Identifier_ClassByte(r);
+    }
+
+    [Benchmark]
+    public Identifier_StructByte Identifier_StructByte()
+    {
+        var r = this.Instance.GetHash(this.ByteArray);
+        return new Identifier_StructByte(r);
+    }
+
+    [GlobalSetup]
+    public void Setup()
+    {
+    }
+
+    [GlobalCleanup]
+    public void Cleanup()
+    {
+    }
+}
---
+++ Benchmark/Design/Identifiers.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+using System;
+using Tinyhand;
+
+namespace Benchmark;
+
+public class Identifier_ClassULong
+{
+    public Identifier_ClassULong()
+    {
+    }
+
+    public Identifier_ClassULong(ulong id0, ulong id1, ulong id2, ulong id3)
+    {
+        this.Id0 = id0;
+        this.Id1 = id1;
+        this.Id2 = id2;
+        this.Id3 = id3;
+    }
+
+    public ulong Id0 { get; set; }
+
+    public ulong Id1 { get; set; }
+
+    public ulong Id2 { get; set; }
+
+    public ulong Id3 { get; set; }
+}
+
+public class Identifier_ClassByte
+{
+    public Identifier_ClassByte()
+    {
+        this.Identifier = new byte[32];
+    }
+
+    public Identifier_ClassByte(byte[] identifier)
+    {
+        this.Identifier = identifier;
+    }
+
+    public byte[] Identifier {  get; set; }
+}
+
+public struct Identifier_StructByte
+{
+    public Identifier_StructByte()
+    {
+        this.Identifier = new byte[32];
+    }
+
+    public Identifier_StructByte(byte[] identifier)
+    {
+        this.Identifier = identifier;
+    }
+
+    public byte[] Identifier { get; set; }
+}
