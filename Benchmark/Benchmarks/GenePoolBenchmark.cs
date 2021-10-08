335a1546c901ba6017d13803c4409d7851ddb51f
archi-Doc <archidoc422@gmail.com>
2021-10-09 01:21:58 +0900

    .

--- Benchmark/Benchmarks/GenePoolBenchmark.cs
+++ Benchmark/Benchmarks/GenePoolBenchmark.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
 
 using System;
+using System.Buffers;
 using System.Security.Cryptography;
 using Arc.Crypto;
 using Benchmark.Design;
@@ -36,6 +37,12 @@ public class GenePoolBenchmark
     }
 
     [Benchmark]
+    public Aes NewAes()
+    {
+        return Aes.Create();
+    }
+
+    [Benchmark]
     public ulong[] Xoshiro_NextULong()
     {
         var array = new ulong[this.Length];
@@ -62,21 +69,21 @@ public class GenePoolBenchmark
     [Benchmark]
     public byte[] GenerateGenePool()
     {
-        return this.GenerateGenePool(this.Source);
+        return this.GenerateGenePool(this.Source, this.Length * sizeof(ulong));
     }
 
     [Benchmark]
     public byte[] GenerateGenePool2()
     {
-        return this.GenerateGenePool2(this.Source);
+        return this.GenerateGenePool2(this.Source, this.Length * sizeof(ulong));
     }
 
-    public unsafe byte[] GenerateGenePool(ReadOnlySpan<byte> source)
+    public unsafe byte[] GenerateGenePool(ReadOnlySpan<byte> source, int destinationSize)
     { // length <= 3k ?, 3k x 8 bytes = 24 kbytes
         var hash = XXHash64.Hash64(source);
         var xo = new Xoshiro256StarStar(hash);
 
-        Span<byte> span = stackalloc byte[source.Length * sizeof(ulong)];
+        Span<byte> span = stackalloc byte[destinationSize];
         xo.NextBytes(span);
 
         var aes = Aes.Create();
@@ -98,29 +105,56 @@ public class GenePoolBenchmark
         // var encryptor = aes.CreateEncryptor(key, iv);
     }
 
-    public unsafe byte[] GenerateGenePool2(ReadOnlySpan<byte> source)
+    public unsafe byte[] GenerateGenePool2(ReadOnlySpan<byte> source, int destinationSize)
     { // length <= 3k ?, 3k x 8 bytes = 24 kbytes
         var hash = XXHash64.Hash64(source);
         var xo = new Xoshiro256StarStar(hash);
 
-        Span<byte> span = stackalloc byte[source.Length * sizeof(ulong)];
+        byte[]? rentBytes = null;
+        Span<byte> span = destinationSize <= 1024 ? stackalloc byte[destinationSize] : (rentBytes = ArrayPool<byte>.Shared.Rent(destinationSize));
         xo.NextBytes(span);
 
         var aes = Aes128.ObjectPool.Get();
-
         try
         {
+            /*xo.NextBytes(aes.Key);
+            xo.NextBytes(aes.IV);
+            var encryptor = aes.Aes.CreateEncryptor(aes.Key, aes.IV);
+
+            var result = new byte[destinationSize];
+            var input = result.AsSpan();
+            var blockSize = aes.Aes.BlockSize;
+            var blocks = destinationSize / blockSize;
+            var inputOffset = 0;
+            for (var i = 0; i < blocks; i++)
+            {
+                encryptor.tr
+                encryptor.TransformBlock(span, inputOffset, destinationSize, result, 0);
+                inputOffset += blockSize;
+                input = input.Slice(blockSize);
+            }*/
+
             xo.NextBytes(aes.Key);
             aes.Aes.Key = aes.Key;
             xo.NextBytes(aes.IV);
+            // aes.Aes.IV = aes.IV;
+            var result = new byte[destinationSize];
+            aes.Aes.TryEncryptCbc(span, aes.IV, result, out var written, PaddingMode.None);
+
+            /*xo.NextBytes(aes.Key);
+            aes.Aes.Key = aes.Key;
+            xo.NextBytes(aes.IV);
             aes.Aes.IV = aes.IV;
-            // var encryptor = aes.Aes.CreateEncryptor(aes.Key, aes.IV);
-            var result = aes.Aes.EncryptCbc(span, aes.IV);
+            var result = aes.Aes.EncryptCbc(span, aes.IV, PaddingMode.None);*/
             return result;
         }
         finally
         {
             Aes128.ObjectPool.Return(aes);
+            if (rentBytes != null)
+            {
+                ArrayPool<byte>.Shared.Return(rentBytes);
+            }
         }
 
         // MemoryMarshal.AsBytes(span);
