155b91281d28b81718c83b583a07b0c799fdbef3
archi-Doc <archidoc422@gmail.com>
2021-11-06 23:05:52 +0900

    .

---
+++ LPBase/Logger/EmptyLogger.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+namespace LP;
+
+public class EmptyLogger : ISimpleLogger
+{
+    public EmptyLogger()
+    {
+    }
+
+    public void Debug(string message)
+    {
+    }
+
+    public void Information(string message)
+    {
+    }
+
+    public void Warning(string message)
+    {
+    }
+
+    public void Error(string message)
+    {
+    }
+
+    public void Fatal(string message)
+    {
+        this.FatalFlag = true;
+    }
+
+    public bool FatalFlag { get; private set; }
+}
