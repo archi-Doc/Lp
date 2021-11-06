a256815988675406b4e538d54ac33c3dfcbb4f6c
archi-Doc <archidoc422@gmail.com>
2021-11-06 23:17:59 +0900

    .

---
+++ Netsphere/Packet/PacketData.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+namespace LP.Net;
+
+[TinyhandObject]
+internal partial class PacketData : IPacket
+{
+    public bool IsResponse => false;
+
+    public PacketId Id => PacketId.Data;
+
+    [Key(0)]
+    public bool Response { get; set; }
+
+    [Key(1)]
+    public ushort NumberOfPackets { get; set; }
+
+    [Key(2)]
+    public uint DataSize { get; set; }
+}
