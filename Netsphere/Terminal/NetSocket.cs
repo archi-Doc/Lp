0ed8e82237f49f896373e5f919afccb97970dc3f
archi-Doc <archidoc422@gmail.com>
2021-11-11 22:38:47 +0900

    .

--- Netsphere/Terminal/NetSocket.cs
+++ Netsphere/Terminal/NetSocket.cs
@@ -93,7 +93,7 @@ public class NetSocket
                 core.ProcessSend();
 
                 // core.Sleep(SendIntervalMilliseconds);
-                core.TryNanoSleep(SendIntervalNanoseconds);
+                core.TryNanoSleep(SendIntervalNanoseconds); // Performs better than core.Sleep() on Linux.
             }
         }
 
