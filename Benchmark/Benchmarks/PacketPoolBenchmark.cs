26c56f31f56407bf9d641549a5feccc342ba481f
archi-Doc <archidoc422@gmail.com>
2021-12-01 23:52:41 +0900

    .

--- Benchmark/Benchmarks/PacketPoolBenchmark.cs
+++ Benchmark/Benchmarks/PacketPoolBenchmark.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Buffers;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,11 +14,22 @@ namespace Benchmark;
 [Config(typeof(BenchmarkConfig))]
 public class PacketPoolBenchmark
 {
+    public const int N = 100;
+
     [Params(2048)]
     public int Length { get; set; }
 
+    public ConcurrentQueue<byte[]> Queue { get; set; } = new();
+
+    public byte[][] Arrays { get; set; }
+
     public PacketPoolBenchmark()
     {
+        this.Arrays = new byte[N][];
+        for (var n = 0; n < N; n++)
+        {
+            this.Queue.Enqueue(new byte[this.Length]);
+        }
     }
 
     [GlobalSetup]
@@ -42,5 +54,67 @@ public class PacketPoolBenchmark
         var array = ArrayPool<byte>.Shared.Rent(this.Length);
         ArrayPool<byte>.Shared.Return(array);
         return array;
+    }
+
+    [Benchmark]
+    public byte[] ConcurrentQueue()
+    {
+        byte[]? array;
+        if (!this.Queue.TryDequeue(out array))
+        {
+            array = new byte[this.Length];
+        }
+
+        this.Queue.Enqueue(array);
+        return array;
+    }
+
+    [Benchmark]
+    public byte[][] NewArrayN()
+    {
+        for (var n = 0; n < N; n++)
+        {
+            this.Arrays[n] = new byte[this.Length];
+        }
+
+        return this.Arrays;
+    }
+
+    [Benchmark]
+    public byte[][] ArrayPoolN()
+    {
+        for (var n = 0; n < N; n++)
+        {
+            this.Arrays[n] = ArrayPool<byte>.Shared.Rent(this.Length);
+        }
+
+        for (var n = 0; n < N; n++)
+        {
+            ArrayPool<byte>.Shared.Return(this.Arrays[n]);
+        }
+
+        return this.Arrays;
+    }
+
+    [Benchmark]
+    public byte[][] ConcurrentQueueN()
+    {
+        for (var n = 0; n < N; n++)
+        {
+            byte[]? array;
+            if (!this.Queue.TryDequeue(out array))
+            {
+                array = new byte[this.Length];
+            }
+
+            this.Arrays[n] = array;
+        }
+
+        for (var n = 0; n < N; n++)
+        {
+            this.Queue.Enqueue(this.Arrays[n]);
+        }
+
+        return this.Arrays;
     }
 }
