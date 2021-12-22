0a815e0269cdbeb2c6bdf6c6b4610507e44ed4fe
archi-Doc <archidoc422@gmail.com>
2021-12-23 01:19:02 +0900

    .

--- Netsphere/Terminal/FlowControl.cs
+++ Netsphere/Terminal/FlowControl.cs
@@ -6,7 +6,7 @@ public class FlowControl
 {
     public static readonly long TicksPerRound = Nsec.FromMilliseconds(2);
     public static readonly double TicksPerRoundRev = 1d / Nsec.FromMilliseconds(2);
-    public static readonly double InitialSendCapacityPerRound = 2;
+    public static readonly double InitialSendCapacityPerRound = 20;
     public static readonly long InitialWindowTicks = Nsec.FromMilliseconds(300);
 
     public FlowControl(NetTerminal netTerminal)
