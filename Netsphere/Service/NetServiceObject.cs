f9da3115cf333ae70905f471fa0c74190a6c7110
archi-Doc <archidoc422@gmail.com>
2022-01-03 22:43:06 +0900

    .

---
+++ Netsphere/Service/NetServiceObject.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+namespace Netsphere;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = false, Inherited = true)]
+public sealed class NetServiceObjectAttribute : Attribute
+{
+    /// <summary>
+    /// Gets or sets an identifier of the net service [0 : auto-generated from the class name].
+    /// </summary>
+    public uint ServiceId { get; set; } = 0;
+
+    public NetServiceObjectAttribute()
+    {
+    }
+}
