b05b609d50615c4bb001585924e0c2b157107bde
archi-Doc <archidoc422@gmail.com>
2022-01-21 17:05:57 +0900

    .

--- xUnitTest/Netsphere/Filters/NullFilter.cs
+++ xUnitTest/Netsphere/Filters/NullFilter.cs
@@ -6,7 +6,7 @@ namespace xUnitTest.Netsphere;
 
 public class NullFilter : IServiceFilter
 {
-    public async Task Invoke(CallContext context, Func<CallContext, Task> next)
+    public async Task Invoke(CallContext context, Func<CallContext, Task> invoker)
     {
         context.Result = NetResult.NoNetService;
     }
--- xUnitTest/Netsphere/Tests/FilterTest.cs
+++ xUnitTest/Netsphere/Tests/FilterTest.cs
@@ -18,10 +18,26 @@ public class FilterTest
     {
         using (var terminal = this.NetControl.Terminal.Create(NodeInformation.Alternative))
         {
-            var basicService = terminal.GetService<IFilterTestService>();
-            var task = await basicService.NoFilter(1).ResponseAsync;
+            var service = terminal.GetService<IFilterTestService>();
+            var task = await service.NoFilter(1).ResponseAsync;
             task.Result.Is(NetResult.Success);
             task.Value.Is(1);
+
+            task = await service.Increment(2).ResponseAsync;
+            task.Result.Is(NetResult.Success);
+            task.Value.Is(3);
+
+            task = await service.Multiply2(3).ResponseAsync;
+            task.Result.Is(NetResult.Success);
+            task.Value.Is(6);
+
+            task = await service.IncrementAndMultiply2(4).ResponseAsync;
+            task.Result.Is(NetResult.Success);
+            task.Value.Is(10);
+
+            task = await service.Multiply2AndIncrement(5).ResponseAsync;
+            task.Result.Is(NetResult.Success);
+            task.Value.Is(11);
         }
     }
 
