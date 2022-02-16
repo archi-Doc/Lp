b3c8e4c2e76e5b229bfe3135898910d82be356b8
archi-Doc <archidoc422@gmail.com>
2022-02-16 23:24:14 +0900

    .

---
+++ ZenItz/Zen/SnowFlake.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+namespace ZenItz;
+
+[TinyhandObject]
+[ValueLinkObject]
+internal partial class Snowflake
+{
+    public Snowflake()
+    {
+    }
+
+    [Key(0)]
+    [Link(Primary = true, Type = ChainType.Unordered)]
+    public uint FlakeId { get; private set; }
+}
