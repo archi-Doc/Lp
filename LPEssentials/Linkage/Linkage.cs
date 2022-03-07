1d54fff6101ca5dca6ae9decaac25995b27661ee
archi-Doc <archidoc422@gmail.com>
2022-03-07 19:22:48 +0900

    .

--- LPEssentials/Linkage/Linkage.cs
+++ LPEssentials/Linkage/Linkage.cs
@@ -8,6 +8,8 @@ namespace LP;
 [TinyhandObject]
 public partial class Linkage : IValidatable // , IEquatable<Linkage>, IComparable<Linkage>
 {
+    public const double MaxPoint = 1.0E15;
+    public const double MinPoint = -1.0E15;
     public const int MaxMergers = 4;
 
     public Linkage()
@@ -16,6 +18,44 @@ public partial class Linkage : IValidatable // , IEquatable<Linkage>, IComparabl
         this.Owner = default!;
         this.Mergers = default!;
         this.Signs = default!;
+    }
+
+    public bool Validate()
+    {
+        if (this.Point < MinPoint || this.Point > MaxPoint)
+        {
+            return false;
+        }
+        else if (this.Owner == null || !this.Owner.Validate())
+        {
+            return false;
+        }
+        else if (this.Mergers == null || this.Mergers.Length > MaxMergers)
+        {
+            return false;
+        }
+        else if (this.Signs == null || this.Signs.Length != (1 + this.Mergers.Length))
+        {
+            return false;
+        }
+
+        for (var i = 0; i < this.Mergers.Length; i++)
+        {
+            if (this.Mergers[i] == null || !this.Mergers[i].Validate())
+            {
+                return false;
+            }
+        }
+
+        for (var i = 0; i < this.Signs.Length; i++)
+        {
+            if (this.Signs[i] == null || this.Signs[i].Length != Authority.PublicKeyLength)
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     [Key(0)]
