17134478cc7ef2c2f3b3024d87c946bfc2060072
archi-Doc <archidoc422@gmail.com>
2022-06-21 23:27:02 +0900

    .

---
+++ LPEssentials/Crypto/KeyVault.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+namespace LP;
+
+public class KeyVault
+{
+}
+
+[TinyhandObject]
+public partial class KeyVaultItem
+{
+    public const int HintLength = 2;
+
+    public KeyVaultItem()
+    {
+    }
+
+    public KeyVaultItem(string name, int hint, byte[] encrypted)
+    {
+        this.Name = name;
+        // Span<byte> span = stackalloc byte[4];
+        // BitConverter.TryWriteBytes(span, hint);
+        // this.Hint = span.Slice(0, HintLength).ToArray();
+        this.Hint = (ushort)hint;
+        this.Encrypted = encrypted;
+    }
+
+    [KeyAsName]
+    public string Name { get; protected set; } = string.Empty;
+
+    [KeyAsName]
+    public ushort Hint { get; protected set; }
+
+    [KeyAsName]
+    public byte[] Encrypted { get; protected set; } = Array.Empty<byte>();
+}
