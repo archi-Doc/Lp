b7f7c23e3c19aff1ec1445c9abaad0a64582b180
archi-Doc <archidoc422@gmail.com>
2022-07-19 18:32:57 +0900

    .

---
+++ LPEssentials/Unit/Logger/UnitLogger.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+namespace Arc.Unit;
+
+public abstract class UnitLogger
+{
+    public IUnitLogger Default { get; }
+
+    public abstract IUnitLogger? TryGet();
+}
+
+public interface IUnitLogger
+{
+    public void Debug(string message);
+
+    public void Information(string message);
+
+    public void Warning(string message);
+
+    public void Error(string message);
+
+    public void Fatal(string message);
+}
