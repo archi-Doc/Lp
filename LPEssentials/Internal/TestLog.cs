630d71533b4823f97dd0933810759744b37d8031
archi-Doc <archidoc422@gmail.com>
2022-08-10 17:12:06 +0900

    .

---
+++ LPEssentials/Internal/TestLog.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+namespace LP;
+
+public static class TestLog
+{
+    public static void Add(string message)
+    {
+        if (LogPath == null)
+        {
+            return;
+        }
+
+        try
+        {
+            File.AppendAllText(LogPath, $"{DateTime.Now.ToString()} {message}\r\n");
+        }
+        catch
+        {
+        }
+    }
+
+    public static string? LogPath { get; set; } = "C:\\App\\Test.log";
+}
