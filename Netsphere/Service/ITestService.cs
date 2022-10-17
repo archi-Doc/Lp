7e2d15c39d8bf60d10ca204966eb8ce5ca5e5eeb
archi-Doc <archidoc422@gmail.com>
2022-10-17 18:58:18 +0900

    .

---
+++ Netsphere/Service/ITestService.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+using Netsphere;
+
+namespace NetsphereTest;
+
+[NetServiceInterface]
+public interface IRemoteControlService : INetService
+{
+    public NetTask RequestAuthorization(Token token);
+
+    public NetTask Restart();
+}
