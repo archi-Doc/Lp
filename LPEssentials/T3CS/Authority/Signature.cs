daf63668eaa8208ed9535eae6aa5c783096187c2
archi-Doc <archidoc422@gmail.com>
2022-10-31 18:57:42 +0900

    .

--- LPEssentials/T3CS/Authority/Signature.cs
+++ LPEssentials/T3CS/Authority/Signature.cs
@@ -24,24 +24,9 @@ public readonly partial struct Signature
         this.SignatureType = signatureType;
         this.SignedMics = signedMics;
 
-        var span = sign.AsSpan();
-        if (span.Length >= PublicKey.PublicKeyLength)
+        if (sign.Length == PublicKey.PublicKeyLength)
         {
-            this.s0 = BitConverter.ToUInt64(span);
-            span = span.Slice(sizeof(ulong));
-            this.s1 = BitConverter.ToUInt64(span);
-            span = span.Slice(sizeof(ulong));
-            this.s2 = BitConverter.ToUInt64(span);
-            span = span.Slice(sizeof(ulong));
-            this.s3 = BitConverter.ToUInt64(span);
-            span = span.Slice(sizeof(ulong));
-            this.s4 = BitConverter.ToUInt64(span);
-            span = span.Slice(sizeof(ulong));
-            this.s5 = BitConverter.ToUInt64(span);
-            span = span.Slice(sizeof(ulong));
-            this.s6 = BitConverter.ToUInt64(span);
-            span = span.Slice(sizeof(ulong));
-            this.s7 = BitConverter.ToUInt64(span);
+            this.sign = sign;
         }
     }
 
@@ -51,39 +36,7 @@ public readonly partial struct Signature
     [Key(1)]
     public readonly long SignedMics;
 
-    // [Key(2)]
-    // [DefaultValue(null)]
-    // private readonly byte[]? sign;
-
     [Key(2)]
-    [DefaultValue(0)]
-    private readonly ulong s0;
-
-    [Key(3)]
-    [DefaultValue(0)]
-    private readonly ulong s1;
-
-    [Key(4)]
-    [DefaultValue(0)]
-    private readonly ulong s2;
-
-    [Key(5)]
-    [DefaultValue(0)]
-    private readonly ulong s3;
-
-    [Key(6)]
-    [DefaultValue(0)]
-    private readonly ulong s4;
-
-    [Key(7)]
-    [DefaultValue(0)]
-    private readonly ulong s5;
-
-    [Key(8)]
-    [DefaultValue(0)]
-    private readonly ulong s6;
-
-    [Key(9)]
-    [DefaultValue(0)]
-    private readonly ulong s7;
+    [DefaultValue(null)]
+    private readonly byte[]? sign;
 }
