0ad78aeafd2c716f51f7941c844d075275508cbd
archi-Doc <archidoc422@gmail.com>
2023-02-03 18:32:33 +0900

    .

--- ZenItz/Zen/Core/FlakeData.cs
+++ ZenItz/Zen/Core/FlakeData.cs
@@ -1,7 +1,5 @@
 ﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
 
-using System.Diagnostics.CodeAnalysis;
-
 namespace ZenItz;
 
 public partial class Zen<TIdentifier>
