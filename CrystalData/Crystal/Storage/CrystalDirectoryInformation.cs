36159b0d256c50690650c3e2f7c0a462cce275f5
archi-Doc <archidoc422@gmail.com>
2023-03-21 23:02:08 +0900

    .

--- CrystalData/Crystal/Storage/CrystalDirectoryInformation.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
-
-namespace CrystalData;
-
-public record CrystalDirectoryInformation(uint DirectoryId, CrystalDirectoryType Type, string DirectoryPath, long DirectoryCapacity, long DirectorySize, double UsageRatio)
-{
-    public override string ToString()
-        => $"Id: {(ushort)this.DirectoryId:x4}, Path: {this.DirectoryPath}, Size/Capacity: {this.DirectorySize}/{this.DirectoryCapacity} ({$"{this.UsageRatio,0:F1}"})";
-}
---
+++ CrystalData/Crystal/Storage/StorageOptions.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+using SimpleCommandLine;
+
+namespace LP.Data;
+
+[TinyhandObject(ImplicitKeyAsName = true)]
+public partial class StorageOptions
+{
+    [SimpleOption("capacity", Description = "Storage capacity in GBs.")]
+    public int Capacity { get; set; } = 10;
+
+    [SimpleOption("path", Required = true)]
+    public string Path { get; set; } = string.Empty;
+
+    [SimpleOption("bucket")]
+    public string Bucket { get; set; } = string.Empty;
+}
