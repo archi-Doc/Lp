300a4524781572a01b369719a4307794fdfd52a6
archi-Doc <archidoc422@gmail.com>
2023-04-03 00:21:46 +0900

    .

---
+++ CrystalData/Crystalizer/Crystal/CrystalDataImpl.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+namespace CrystalData;
+
+internal class CrystalDataImpl<TData> : CrystalImpl<TData>
+    where TData : ITinyhandSerialize<TData>, ITinyhandReconstruct<TData>
+{
+    public CrystalDataImpl(Crystalizer crystalizer)
+        : base(crystalizer)
+    {
+    }
+}
