5bb9fb8d1b50e331ef126f52d7d4de3df220a6fe
archi-Doc <archidoc422@gmail.com>
2023-08-14 15:27:19 +0900

    .

--- xUnitTest/CrystalData/JournalTest.cs
+++ xUnitTest/CrystalData/JournalTest.cs
@@ -6,7 +6,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Tinyhand;
 using ValueLink;
 using Xunit;
-using static CrystalData.CrystalDataHashed;
 
 namespace xUnitTest.CrystalDataTest;
 
@@ -42,13 +41,44 @@ internal partial record SerializableData : IEquatableObject<SerializableData>
         => this.id == other.id && this.name == other.name && this.age == other.age;
 }
 
+[TinyhandObject(Journal = true)]
+[ValueLinkObject(Isolation = IsolationLevel.RepeatableRead)]
+internal partial record RepeatableData : IEquatableObject<RepeatableData>
+{
+    public RepeatableData()
+    {
+    }
+
+    public RepeatableData(int id, string name, double age)
+    {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    [Key(0, AddProperty = "Id")]
+    [Link(Primary = true, Unique = true, Type = ChainType.Ordered)]
+    private int id;
+
+    [Key(1, AddProperty = "Name")]
+    private string name = string.Empty;
+
+    [Key(2, AddProperty = "Age")]
+    private double age;
+
+    public override string ToString()
+        => $"{this.id} {this.name} ({this.age.ToString()})";
+
+    public bool ObjectEquals(RepeatableData other)
+        => this.id == other.id && this.name == other.name && this.age == other.age;
+}
+
 public class JournalTest
 {
     [Fact]
     public async Task TestSerializable()
     {
-        // var tester = new JournalTester();
-        var c = await this.CreateAndStartCrystal();
+        var c = await this.CreateAndStartCrystal<SerializableData.GoshujinClass>();
         var g1 = c.Data;
         lock (g1.SyncObject)
         {
@@ -137,7 +167,103 @@ public class JournalTest
         await TestHelper.UnloadAndDeleteAll(c);
     }
 
-    private async Task<ICrystal<SerializableData.GoshujinClass>> CreateAndStartCrystal()
+    [Fact]
+    public async Task TestRepeatable()
+    {
+        var c = await this.CreateAndStartCrystal<RepeatableData.GoshujinClass>();
+        var g1 = c.Data;
+        lock (g1.SyncObject)
+        {
+        }
+
+        await c.Save(true);
+        await c.Crystalizer.SaveJournal();
+
+        // g2: empty
+        await c.PrepareAndLoad(false);
+        var g2 = c.Data;
+        g2.GoshujinEquals(g1).IsTrue();
+
+        g2.Count.Is(0);
+        g2.Add(new(0, "Zero", 0));
+
+        await c.Save(true);
+        await c.Crystalizer.SaveJournal();
+
+        // g3: Zero
+        await c.PrepareAndLoad(false);
+        var g3 = c.Data;
+        g3.GoshujinEquals(g2).IsTrue();
+        {
+            g3.Count.Is(1);
+            var d = g3.TryGet(0)!;
+            d.IsNotNull();
+            d.Id.Is(0);
+            d.Name.Is("Zero");
+            d.Age.Is(0d);
+        }
+
+        await c.Save(true);
+        await c.Crystalizer.SaveJournal();
+        var result = await c.Crystalizer.TestJournalAll();
+        result.IsTrue();
+
+        // g4: 1, 2, 3, 4
+        await c.PrepareAndLoad(false);
+        var g4 = c.Data;
+        g4.GoshujinEquals(g3).IsTrue();
+        {
+            g4.Add(new(1, "1", 1d));
+            g4.Add(new(4, "4", 4d));
+            g4.Add(new(3, "3", 3d));
+            g4.Add(new(2, "2", 2d));
+
+            using (var w = g4.TryLock(3)!)
+            {
+                w.Goshujin = null;
+                w.Commit();
+            }
+
+            using (var w = g4.TryLock(0)!)
+            {
+                w.Id = 100;
+                w.Commit();
+            }
+        }
+
+        await c.Save(true);
+        await c.Crystalizer.SaveJournal();
+        result = await c.Crystalizer.TestJournalAll();
+        result.IsTrue();
+
+        // g5
+        await c.PrepareAndLoad(false);
+        var g5 = c.Data;
+        g5.GoshujinEquals(g4).IsTrue();
+        {
+            using (var w = g5.TryLock(1)!)
+            {
+                w.Name = "One";
+                w.Commit();
+            }
+
+            using (var w = g5.TryLock(100)!)
+            {
+                w.Name = "100";
+                w.Commit();
+            }
+        }
+
+        await c.Save(true);
+        await c.Crystalizer.SaveJournal();
+        result = await c.Crystalizer.TestJournalAll();
+        result.IsTrue();
+
+        await TestHelper.UnloadAndDeleteAll(c);
+    }
+
+    private async Task<ICrystal<TData>> CreateAndStartCrystal<TData>()
+        where TData : class, ITinyhandSerialize<TData>, ITinyhandReconstruct<TData>
     {
         var builder = new CrystalControl.Builder();
         builder
@@ -145,7 +271,7 @@ public class JournalTest
             {
                 var directory = $"Crystal[{RandomVault.Pseudo.NextUInt32():x4}]";
                 context.SetJournal(new SimpleJournalConfiguration(new LocalDirectoryConfiguration(Path.Combine(directory, "Journal"))));
-                context.AddCrystal<SerializableData.GoshujinClass>(
+                context.AddCrystal<TData>(
                     new(SavePolicy.Manual, new LocalFileConfiguration(Path.Combine(directory, "Test.tinyhand")))
                     {
                         SaveFormat = SaveFormat.Utf8,
@@ -156,7 +282,7 @@ public class JournalTest
         var unit = builder.Build();
         var crystalizer = unit.Context.ServiceProvider.GetRequiredService<Crystalizer>();
 
-        var crystal = crystalizer.GetCrystal<SerializableData.GoshujinClass>();
+        var crystal = crystalizer.GetCrystal<TData>();
         var result = await crystalizer.PrepareAndLoadAll(false);
         result.Is(CrystalResult.Success);
         return crystal;
