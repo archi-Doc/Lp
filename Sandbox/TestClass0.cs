3c912ba73c0f04b286bc52dddf84972414f326d2
archi-Doc <archidoc422@gmail.com>
2023-10-14 12:07:32 +0900

    .

--- Sandbox/TestClass0.cs
+++ Sandbox/TestClass0.cs
@@ -74,6 +74,7 @@ internal class TestClass0
             if (w is not null)
             {
                 w.Name = "One";
+                w.Children = default!;
                 w.Commit();
             }
         }
