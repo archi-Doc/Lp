aed726483198781aeab94c7bfe33c01ab12bb8da
archi-Doc <archidoc422@gmail.com>
2023-12-07 00:32:17 +0900

    .

--- Netsphere/Net/Frame/GeneFrame.cs
+++ Netsphere/Net/Frame/GeneFrame.cs
@@ -16,3 +16,25 @@ internal readonly struct GeneFrame
     public readonly uint GenePosition; // 4 bytes
     public readonly uint GeneTotal; // 4 bytes
 }
+
+internal readonly struct FirstGeneFrame
+{// FirstGeneFrameCode
+    public const int Length = 22;
+    public const int MaxGeneLength = PacketHeader.MaxFrameLength - Length;
+
+    public readonly FrameType FrameType; // 2 bytes
+    public readonly uint TransmissionId; // 4 bytes
+    public readonly uint GeneTotal; // 4 bytes
+    public readonly uint PrimaryId; // 4 bytes
+    public readonly ulong SecondaryId; // 8 bytes
+}
+
+internal readonly struct FollowingGeneFrame
+{// FollowingGeneFrameCode
+    public const int Length = 10;
+    public const int MaxGeneLength = PacketHeader.MaxFrameLength - Length;
+
+    public readonly FrameType FrameType; // 2 bytes
+    public readonly uint TransmissionId; // 4 bytes
+    public readonly uint GenePosition; // 4 bytes
+}
