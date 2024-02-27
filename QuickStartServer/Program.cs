04e76f1c3830a7608492966ec7badd575ab23d08
archi-Doc <archidoc422@gmail.com>
2024-02-27 22:04:52 +0900

    .

---
+++ QuickStartServer/Program.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+using Arc.Threading;
+using Microsoft.Extensions.DependencyInjection;
+using Netsphere;
+
+namespace QuickStartServer;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        AppDomain.CurrentDomain.ProcessExit += (s, e) =>
+        {// Console window closing or process terminated.
+            ThreadCore.Root.Terminate(); // Send a termination signal to the root.
+            ThreadCore.Root.TerminationEvent.WaitOne(2000); // Wait until the termination process is complete (#1).
+        };
+
+        Console.CancelKeyPress += (s, e) =>
+        {// Ctrl+C pressed
+            e.Cancel = true;
+            ThreadCore.Root.Terminate(); // Send a termination signal to the root.
+        };
+
+        var builder = new NetControl.Builder()
+            .SetupOptions<NetOptions>((context, options) =>
+            {// NetsphereOptions
+                options.NodeName = "Test server";
+                options.Port = 49152;
+                options.EnableEssential = true;
+                options.EnableServer = true;
+            });
+
+        // Netsphere
+        var unit = builder.Build();
+        var options = unit.Context.ServiceProvider.GetRequiredService<NetOptions>();
+        await Console.Out.WriteLineAsync(options.ToString());
+
+        var netBase = unit.Context.ServiceProvider.GetRequiredService<NetBase>();
+
+        await unit.Run(options, true);
+
+        await Console.Out.WriteLineAsync();
+        await Console.Out.WriteLineAsync("Server started.");
+        await Console.Out.WriteLineAsync("Ctrl+C to exit.");
+        while (await ThreadCore.Root.Delay(1_000))
+        {
+        }
+
+        await unit.Terminate();
+
+        ThreadCore.Root.Terminate();
+        await ThreadCore.Root.WaitForTerminationAsync(-1); // Wait for the termination infinitely.
+        ThreadCore.Root.TerminationEvent.Set(); // The termination process is complete (#1).
+    }
+}
---
+++ QuickStartServer/TestService.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+using Netsphere;
+
+namespace QuickStart;
+
+[NetServiceInterface]
+public interface TestService : INetService
+{
+    NetTask<string?> DoubleString(string source);
+}
+
+internal class TestServiceImpl : TestService
+{
+    async NetTask<string?> TestService.DoubleString(string source)
+        => source + source;
+}
