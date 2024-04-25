e97d8acfe972f95790437a751f1092bdc73ffd13
archi-Doc <archidoc422@gmail.com>
2024-04-25 22:46:26 +0900

    .

--- Netsphere/Relay/Agent/RelayAgent.cs
+++ Netsphere/Relay/Agent/RelayAgent.cs
@@ -11,64 +11,6 @@ namespace Netsphere.Relay;
 /// </summary>
 public partial class RelayAgent
 {
-    [ValueLinkObject]
-    private partial class RelayExchange
-    {
-        public enum Type
-        {
-            Inner,
-            Outer,
-        }
-
-        public RelayExchange(ushort relayId, ServerConnection serverConnection)
-        {
-            this.RelayId = relayId;
-            this.Endpoint = serverConnection.DestinationEndpoint;
-
-            this.Key = new byte[Connection.EmbryoKeyLength];
-            serverConnection.UnsafeCopyKey(this.Key);
-            this.Iv = new byte[Connection.EmbryoIvLength];
-            serverConnection.UnsafeCopyIv(this.Iv);
-        }
-
-        // public Type RelayType { get; private set; }
-
-        [Link(Primary = true, Type = ChainType.Unordered, AddValue = false)]
-        public ushort RelayId { get; private set; }
-
-        [Link(UnsafeTargetChain = "RelayIdChain", AutoLink = false, AddValue = false)]
-        public ushort OuterRelayId { get; private set; }
-
-        // [Link(Type = ChainType.Unordered, AddValue = false)]
-        public NetEndpoint Endpoint { get; private set; }
-
-        public NetEndpoint OuterEndpoint { get; private set; }
-
-        public long RelayPoint { get; private set; } = 1_000_000; // tempcode
-
-        internal byte[] Key { get; private set; }
-
-        internal byte[] Iv { get; private set; }
-
-        public bool DecrementAndCheck()
-        {
-            if (this.RelayPoint-- <= 0)
-            {
-                this.Clean();
-                this.Goshujin = null;
-                return false;
-            }
-            else
-            {
-                return true;
-            }
-        }
-
-        public void Clean()
-        {
-        }
-    }
-
     internal RelayAgent(IRelayControl relayControl, NetTerminal netTerminal)
     {
         this.relayControl = relayControl;
--- Netsphere/Relay/Block/CreateRelayBlock.cs
+++ Netsphere/Relay/Block/CreateRelayBlock.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
 
-namespace Netsphere.Packet;
+namespace Netsphere.Relay;
 
 [TinyhandObject(ReservedKeys = 2)]
 public partial class CreateRelayBlock
