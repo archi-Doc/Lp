c190bfa8f98740d9131e7a5e2941c28bbd00a2ed
archi-Doc <archidoc422@gmail.com>
2024-06-14 23:35:55 +0900

    .

--- Netsphere.Version/Commands/RestartCommand.cs
+++ Netsphere.Version/Commands/RestartCommand.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
 
+using Netsphere;
 using Netsphere.Crypto;
 using Netsphere.Interfaces;
 using Netsphere.Packet;
@@ -9,7 +10,7 @@ namespace LP.Subcommands;
 [SimpleCommand("restart")]
 public class RestartCommand : ISimpleCommandAsync<RestartOptions>
 {
-    private const int WaitIntervalInSeconds = 10;
+    private const int WaitIntervalInSeconds = 20;
     private const int PingIntervalInSeconds = 1;
     private const int PingRetries = 5;
 
@@ -40,6 +41,7 @@ public class RestartCommand : ISimpleCommandAsync<RestartOptions>
             }
         }
 
+        var success = 0;
         await Parallel.ForEachAsync(nodeList, async (netNode, cancellationToken) =>
         {
             // Ping container
@@ -57,53 +59,44 @@ public class RestartCommand : ISimpleCommandAsync<RestartOptions>
                     this.logger.TryGet()?.Log($"Could not connect {netNode.ToString()}");
                     return;
                 }
+
+                var token = new AuthenticationToken(connection.Salt);
+                NetHelper.Sign(token, privateKey);
+                var result = await connection.SetAuthenticationToken(token).ConfigureAwait(false);
+                if (result != NetResult.Success)
+                {
+                    return;
+                }
+
+                var service = connection.GetService<IRemoteControl>();
+                result = await service.Restart();
+                this.logger.TryGet()?.Log($"Restart({result}): {netNode.Address.ToString()}");
+                if (result != NetResult.Success)
+                {
+                    return;
+                }
+            }
+
+            // Wait
+            // this.logger.TryGet()?.Log($"Waiting...");
+            await Task.Delay(TimeSpan.FromSeconds(WaitIntervalInSeconds));
+
+            // Ping container
+            var sec = PingIntervalInSeconds;
+            for (var i = 0; i < PingRetries; i++)
+            {
+                if (await this.Ping(address))
+                {
+                    success++;
+                    return;
+                }
+
+                await Task.Delay(TimeSpan.FromSeconds(sec));
+                sec *= 2;
             }
         });
 
-        /*
-        // Restart
-        using (var connection = await this.netTerminal.Connect(netNode))
-        {
-            if (connection == null)
-            {
-                this.logger.TryGet()?.Log($"Could not connect {netNode.ToString()}");
-                return;
-            }
-
-            var token = new AuthenticationToken(connection.Salt);
-            NetHelper.Sign(token, privateKey);
-            var result = await connection.SetAuthenticationToken(token).ConfigureAwait(false);
-            if (result != NetResult.Success)
-            {
-                this.logger.TryGet(LogLevel.Error)?.Log("Error.Authorization");
-                return;
-            }
-
-            var service = connection.GetService<IRemoteControl>();
-            result = await service.Restart();
-            this.logger.TryGet()?.Log($"Restart: {result}");
-            if (result != NetResult.Success)
-            {
-                return;
-            }
-        }
-
-        // Wait
-        this.logger.TryGet()?.Log($"Waiting...");
-        await Task.Delay(TimeSpan.FromSeconds(WaitIntervalInSeconds));
-
-        // Ping container
-        var sec = PingIntervalInSeconds;
-        for (var i = 0; i < PingRetries; i++)
-        {
-            if (await this.Ping())
-            {
-                return;
-            }
-
-            await Task.Delay(TimeSpan.FromSeconds(sec));
-            sec *= 2;
-        }*/
+        this.logger.TryGet()?.Log($"Restart Success/Total: {success}/{nodeList.Count}");
     }
 
     private async Task<bool> Ping(NetAddress address)
