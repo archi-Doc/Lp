0ddcc4778d7e440bd509698b21445e0ca3bb4803
archi-Doc <archidoc422@gmail.com>
2024-06-18 17:32:14 +0900

    .

---
+++ Lp/T3cs/Core/Proof/CredentialProof.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+namespace Lp.T3cs;
+
+[TinyhandObject]
+public sealed partial class CredentialProof : Proof
+{
+    public CredentialProof()
+    {
+    }
+
+    #region FieldAndProperty
+
+    [Key(5)]
+    public Value Value { get; private set; }
+
+    [Key(6)]
+    public NetAddress NetAddress { get; private set; }
+
+    #endregion
+
+    public override bool Validate()
+    {
+        if (!base.Validate())
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public bool ValidateAndVerify()
+    {
+        return LP.TinyhandHelper.ValidateAndVerify(this);
+    }
+}
