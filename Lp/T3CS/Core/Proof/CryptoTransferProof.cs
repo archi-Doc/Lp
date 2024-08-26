37657f0d6498d11a6d46909e6d29c2260f2ff174
archi-Doc <archidoc422@gmail.com>
2024-08-27 00:14:16 +0900

    .

--- Lp/T3CS/Core/Proof/CryptoTransferProof.cs
+++ Lp/T3CS/Core/Proof/CryptoTransferProof.cs
@@ -5,7 +5,7 @@ using Netsphere.Crypto;
 namespace Lp.T3cs;
 
 [TinyhandObject]
-public partial class CryptoTransferProof : Proof
+public sealed partial class CryptoTransferProof : Proof
 {
     private const int OriginalKeyLevel = 1_000;
 
@@ -18,22 +18,28 @@ public partial class CryptoTransferProof : Proof
 
     public SignaturePublicKey SenderKey => this.PublicKey;
 
-    // CryptoSeed, EncryptionPublicKey, Validation
+    // Encryption, (PublicKey), Encrypted
 
     [Key(Proof.ReservedKeyCount + 1)]
-    public SignaturePublicKey SenderCryptoKey { get; protected set; }
+    public uint SenderEncryption { get; private set; }
 
-    [Key(Proof.ReservedKeyCount + 2, Level = OriginalKeyLevel)]
-    public SignaturePublicKey SenderOriginalKey { get; protected set; }
+    [Key(Proof.ReservedKeyCount + 2)]
+    public byte[]? SenderEncrypted { get; private set; }
 
-    [Key(Proof.ReservedKeyCount + 3)]
-    public SignaturePublicKey RecipientKey { get; protected set; }
+    [Key(Proof.ReservedKeyCount + 3, Level = OriginalKeyLevel)]
+    public SignaturePublicKey SenderOriginalKey { get; private set; }
 
     [Key(Proof.ReservedKeyCount + 4)]
-    public SignaturePublicKey RecipientCryptoKey { get; protected set; }
+    public uint RecipientEncryption { get; private set; }
 
-    [Key(Proof.ReservedKeyCount + 5, Level = OriginalKeyLevel)]
-    public SignaturePublicKey RecipientOriginalKey { get; protected set; }
+    [Key(Proof.ReservedKeyCount + 5)]
+    public SignaturePublicKey RecipientKey { get; private set; }
+
+    [Key(Proof.ReservedKeyCount + 6)]
+    public byte[]? RecipientEncrypted { get; private set; }
+
+    [Key(Proof.ReservedKeyCount + 7, Level = OriginalKeyLevel)]
+    public SignaturePublicKey RecipientOriginalKey { get; private set; }
 
     public bool TryGetSenderKey(out SignaturePublicKey senderKey)
     {
