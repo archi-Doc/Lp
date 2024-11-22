6cf9b001f3a3ad3ae67e5a8da33437c63ad15e98
archi-Doc <archidoc422@gmail.com>
2024-11-22 18:08:26 +0900

    .................

--- Lp/SubCommands/Authority/RemoveAuthoritySubcommand.cs
+++ Lp/SubCommands/Authority/RemoveAuthoritySubcommand.cs
@@ -8,14 +8,28 @@ namespace Lp.Subcommands.AuthorityCommand;
 [SimpleCommand("remove-authority")]
 public class RemoveAuthoritySubcommand : ISimpleCommandAsync<AuthoritySubcommandNameOptions>
 {
-    public RemoveAuthoritySubcommand(ILogger<RemoveAuthoritySubcommand> logger, AuthorityControl2 authorityControl)
+    public RemoveAuthoritySubcommand(ILogger<RemoveAuthoritySubcommand> logger, AuthorityControl2 authorityControl, IUserInterfaceService userInterfaceService)
     {
         this.authorityControl = authorityControl;
         this.logger = logger;
+        this.userInterfaceService = userInterfaceService;
     }
 
     public async Task RunAsync(AuthoritySubcommandNameOptions option, string[] args)
     {
+        if (!this.authorityControl.Exists(option.AuthorityName))
+        {// Not found
+            this.logger.TryGet()?.Log(Hashed.Authority.NotFound, option.AuthorityName);
+            return;
+        }
+        else
+        {
+            if (await this.userInterfaceService.RequestYesOrNo(Hashed.Authority.DeleteConfirm, option.AuthorityName) != true)
+            {
+                return;
+            }
+        }
+
         var result = this.authorityControl.RemoveAuthority(option.AuthorityName);
 
         if (result == AuthorityResult.Success)
@@ -30,6 +44,7 @@ public class RemoveAuthoritySubcommand : ISimpleCommandAsync<AuthoritySubcommand
 
     private readonly AuthorityControl2 authorityControl;
     private readonly ILogger logger;
+    private readonly IUserInterfaceService userInterfaceService;
 }
 
 public record AuthoritySubcommandNameOptions
