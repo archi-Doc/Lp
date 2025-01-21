747e756baad0328ef5d2b07e4ed301a615aa080c
archi-Doc <archidoc422@gmail.com>
2025-01-21 10:37:29 +0900

    .

--- Lp/Machines/LPControlMachine.cs
+++ Lp/Machines/LPControlMachine.cs
@@ -7,7 +7,17 @@ namespace Lp.Machines;
 [MachineObject(UseServiceProvider = true)]
 public partial class LpControlMachine : Machine
 {
-    private const int IntervalInSeconds = 10;
+    private const int IntervalInSeconds = 1;
+    private const int DisplayIntervalInSeconds = 10;
+
+    #region FieldAndProperty
+
+    private readonly ILogger logger;
+    private readonly Control control;
+    private readonly LpOptions options;
+    private long lifespan;
+
+    #endregion
 
     public LpControlMachine(ILogger<LpControlMachine> logger, Control control, LpOptions options)
     {
@@ -26,27 +36,33 @@ public partial class LpControlMachine : Machine
     [StateMethod(0)]
     protected StateResult Initial(StateParameter parameter)
     {
+        var result = this.ProcessLifespan();
+        if (result != StateResult.Continue)
+        {
+            return result;
+        }
+
+        return StateResult.Continue;
+    }
+
+    private StateResult ProcessLifespan()
+    {
         if (this.lifespan >= 0)
         {
             this.lifespan -= IntervalInSeconds;
-            if (this.lifespan < 0)
+            if (this.lifespan <= 0)
             {
                 this.logger.TryGet(LogLevel.Warning)?.Log($"Lp is terminating because the specified time has elapsed.");
                 _ = this.control.TryTerminate(true);
                 return StateResult.Terminate;
             }
-            else
+            else if (this.lifespan % DisplayIntervalInSeconds == 0)
             {
-                var x = (this.lifespan + IntervalInSeconds) / IntervalInSeconds * IntervalInSeconds;
+                var x = this.lifespan / DisplayIntervalInSeconds * DisplayIntervalInSeconds;
                 this.logger.TryGet(LogLevel.Information)?.Log($"Lp will terminate in {x} seconds.");
             }
         }
 
         return StateResult.Continue;
     }
-
-    private readonly ILogger logger;
-    private readonly Control control;
-    private readonly LpOptions options;
-    private long lifespan;
 }
