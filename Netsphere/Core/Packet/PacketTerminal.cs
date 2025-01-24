c6e7ddf4878cc96a894555908d7c9cade05243a1
archi-Doc <archidoc422@gmail.com>
2025-01-24 16:19:08 +0900

    .

--- Netsphere/Core/Packet/PacketTerminal.cs
+++ Netsphere/Core/Packet/PacketTerminal.cs
@@ -386,18 +386,18 @@ public sealed partial class PacketTerminal
                     if (TinyhandSerializer.TryDeserialize<PunchPacket>(span, out var p) &&
                         p.DestinationEndpoint.IsValid)
                     {
-                        Console.WriteLine($"PunchPacket {p.ToString()}");
+                        // Console.WriteLine($"PunchPacket {p.ToString()}");
                         if (p.RelayEndpoint.IsValid)
                         {// Relay
-                            var packet = new PunchPacket(default, endpoint);
+                            var packet = new PunchPacket(default, p.DestinationEndpoint);
                             CreatePacket(packetId, packet, out var rentMemory); // CreatePacketCode (no relay)
                             this.SendPacketWithoutRelay(p.RelayEndpoint, rentMemory, default);
                         }
                         else
-                        {//
+                        {// Response
                             var packet = new PunchPacketResponse();
                             CreatePacket(packetId, packet, out var rentMemory); // CreatePacketCode (no relay)
-                            this.SendPacketWithoutRelay(endpoint, rentMemory, default);
+                            this.SendPacketWithoutRelay(p.DestinationEndpoint, rentMemory, default);
                         }
                     }
                 }
