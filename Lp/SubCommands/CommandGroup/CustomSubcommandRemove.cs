de01bcc5b39460dcd8f47d5db624597d7c98345f
archi-Doc <archidoc422@gmail.com>
2025-02-10 21:06:49 +0900

    .

---
+++ Lp/SubCommands/CommandGroup/CustomSubcommandRemove.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+using Lp.Services;
+using SimpleCommandLine;
+
+namespace Lp.Subcommands;
+
+public partial class CommandGroup
+{
+    [SimpleCommand("rm")]
+    public class CustomSubcommandRemove : ISimpleCommandAsync<CustomSubcommandNameOptions>
+    {
+        public CustomSubcommandRemove(ILogger<CustomSubcommandRemove> logger, VaultControl vaultControl)
+        {
+            this.vaultControl = vaultControl;
+            this.logger = logger;
+        }
+
+        public async Task RunAsync(CustomSubcommandNameOptions option, string[] args)
+        {
+            var name = CustomizedCommand.GetName(option.Name);
+
+            if (this.vaultControl.Root.Remove(name))
+            {
+                this.logger.TryGet()?.Log(Hashed.Custom.Removed, option.Name);
+            }
+            else
+            {
+                this.logger.TryGet(LogLevel.Warning)?.Log(Hashed.Custom.NotFound, option.Name);
+            }
+        }
+
+        private VaultControl vaultControl;
+        private ILogger<CustomSubcommandRemove> logger;
+    }
+
+    public record CustomSubcommandNameOptions
+    {
+        [SimpleOption("Name", Description = "Customized command name", Required = true)]
+        public string Name { get; init; } = string.Empty;
+    }
+}
---
+++ Lp/SubCommands/CommandGroup/ListCommand.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+using Lp.Services;
+using SimpleCommandLine;
+
+namespace Lp.Subcommands;
+
+public partial class CommandGroup
+{
+    [SimpleCommand("list-command-group")]
+    public class ListCommand : ISimpleCommandAsync
+    {
+        public ListCommand(VaultControl vaultControl, IUserInterfaceService userInterfaceService)
+        {
+            this.vaultControl = vaultControl;
+            this.userInterfaceService = userInterfaceService;
+        }
+
+        public async Task RunAsync(string[] args)
+        {
+            var names = this.vaultControl.Root.GetNames(CommandGroup.Prefix).Select(x => x.Substring(CommandGroup.Prefix.Length)).ToArray();
+            this.userInterfaceService.WriteLine(string.Join(' ', names));
+        }
+
+        private readonly VaultControl vaultControl;
+        private readonly IUserInterfaceService userInterfaceService;
+    }
+}
