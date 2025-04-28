13bbf432d688fa7832775936e484273c0ceaeb40
archi-Doc <archidoc422@gmail.com>
2025-04-29 01:24:49 +0900

    .

--- Lp/Services/LpDogma/LpDogmaGetInformationSubcommand.cs
+++ Lp/Services/LpDogma/LpDogmaGetInformationSubcommand.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
 
+using System.Diagnostics.CodeAnalysis;
 using Lp.Services;
 using Lp.T3cs;
 using Netsphere.Crypto;
@@ -7,9 +8,26 @@ using SimpleCommandLine;
 
 namespace Lp.Subcommands;
 
+public readonly record struct ConnectionAndService<TService>(Connection? Connection, TService? Service) : IDisposable
+    where TService : INetService
+{
+    [MemberNotNullWhen(true, nameof(Connection))]
+    [MemberNotNullWhen(true, nameof(Service))]
+    public bool IsValid => this.Connection is not null && this.Service is not null;
+
+    [MemberNotNullWhen(false, nameof(Connection))]
+    [MemberNotNullWhen(false, nameof(Service))]
+    public bool IsInvalid => this.Connection is null || this.Service is null;
+
+    public void Dispose()
+    {
+        this.Connection?.Dispose();
+    }
+}
+
 internal static class LpDogmaHelper
 {
-    public static async Task<LpDogmaNetService?> TryConnect(ILogger logger, AuthorityControl authorityControl, NetTerminal netTerminal, string netNode)
+    public static async Task<ConnectionAndService<LpDogmaNetService>> TryConnect(ILogger logger, AuthorityControl authorityControl, NetTerminal netTerminal, string netNode)
     {
         if (await authorityControl.GetLpSeedKey(logger) is not { } lpSeedKey)
         {
@@ -21,19 +39,23 @@ internal static class LpDogmaHelper
             return default;
         }
 
-        using (var connection = await netTerminal.Connect(node))
+        var connection = await netTerminal.Connect(node);
+        if (connection is null)
         {
-            if (connection is null)
-            {
-                logger.TryGet(LogLevel.Error)?.Log(Hashed.Error.Connect, netNode.ToString());
-                return default;
-            }
-
-            var service = connection.GetService<LpDogmaNetService>();
-            var auth = AuthenticationToken.CreateAndSign(lpSeedKey, connection);
-            var r = await service.Authenticate(auth);
-            return service;
+            logger.TryGet(LogLevel.Error)?.Log(Hashed.Error.Connect, netNode.ToString());
+            return default;
         }
+
+        var service = connection.GetService<LpDogmaNetService>();
+        var auth = AuthenticationToken.CreateAndSign(lpSeedKey, connection);
+        var r = await service.Authenticate(auth);
+        if (r.Result.IsError())
+        {
+            connection.Dispose();
+            return default;
+        }
+
+        return new(connection, service);
     }
 }
 
@@ -49,38 +71,40 @@ public class LpDogmaGetInformationSubcommand : ISimpleCommandAsync<ConnectNetNod
 
     public async Task RunAsync(ConnectNetNodeOptions options, string[] args)
     {
-        var service = await LpDogmaHelper.TryConnect(this.logger, this.authorityControl, this.netTerminal, options.NetNode);
-        if (service is null)
+        using (var connectionAndService = await LpDogmaHelper.TryConnect(this.logger, this.authorityControl, this.netTerminal, options.NetNode))
         {
-            return;
-        }
+            if (connectionAndService.IsInvalid)
+            {
+                return;
+            }
 
-        var info = await service.GetInformation();
-        if (info is null)
-        {
-            return;
-        }
+            var info = await connectionAndService.Service.GetInformation();
+            if (info is null)
+            {
+                return;
+            }
 
-        this.logger.TryGet()?.Log($"Success");
+            this.logger.TryGet()?.Log($"Success");
 
-        if (info.NodeKey.IsValid)
-        {
-            this.logger.TryGet()?.Log($"NodeKey: {info.NodeKey}");
-        }
+            if (info.NodeKey.IsValid)
+            {
+                this.logger.TryGet()?.Log($"NodeKey: {info.NodeKey}");
+            }
 
-        if (info.MergerKey.IsValid)
-        {
-            this.logger.TryGet()?.Log($"MergerKey: {info.MergerKey}");
-        }
+            if (info.MergerKey.IsValid)
+            {
+                this.logger.TryGet()?.Log($"MergerKey: {info.MergerKey}");
+            }
 
-        if (info.RelayMergerKey.IsValid)
-        {
-            this.logger.TryGet()?.Log($"RelayMergerKey: {info.RelayMergerKey}");
-        }
+            if (info.RelayMergerKey.IsValid)
+            {
+                this.logger.TryGet()?.Log($"RelayMergerKey: {info.RelayMergerKey}");
+            }
 
-        if (info.LinkerKey.IsValid)
-        {
-            this.logger.TryGet()?.Log($"LinkerKey: {info.LinkerKey}");
+            if (info.LinkerKey.IsValid)
+            {
+                this.logger.TryGet()?.Log($"LinkerKey: {info.LinkerKey}");
+            }
         }
     }
 
