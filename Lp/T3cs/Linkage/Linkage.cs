832a80745b0518facf941036ff5bd666c0d638fa
archi-Doc <archidoc422@gmail.com>
2025-05-11 17:09:57 +0900

    .

--- Lp/T3cs/Linkage/Linkage.cs
+++ Lp/T3cs/Linkage/Linkage.cs
@@ -1,20 +1,39 @@
 ﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
 
+using Netsphere.Crypto;
+
 namespace Lp.T3cs;
 
 [TinyhandObject]
-public sealed partial class Linkage<TProof>
+public sealed partial class LinkerProof : ProofAndPublicKey
+{
+    public override bool Validate()
+    {
+        if (!base.Validate())
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
+
+/*[TinyhandObject]
+public partial class LinkerLinkage : Linkage<LinkerProof>
+{
+}*/
+
+[TinyhandObject]
+public partial class Linkage<TProof> : IValidatable
     where TProof : Proof
 {
-    private Linkage()
-    {
-    }
+    #region FieldAndProperty
 
     [Key(0)]
-    public Proof ProofA { get; set; } = default!;
+    public TProof ProofA { get; set; }
 
     [Key(1)]
-    public Proof ProofB { get; set; } = default!;
+    public TProof ProofB { get; set; }
 
     [Key(2)]
     private byte[]? mergerSignatureA1;
@@ -36,6 +55,34 @@ public sealed partial class Linkage<TProof>
 
     [Key(8)]
     private byte[]? linkerSignature;
+
+    #endregion
+
+    public Linkage(TProof proofA, TProof proofB)
+    {
+        this.ProofA = proofA;
+        this.ProofB = proofB;
+    }
+
+    public bool Validate()
+    {
+        SignaturePublicKey linkerPublicKey;
+        if (!this.ProofA.Validate() ||
+            !this.ProofA.TryGetLinkerPublicKey(out linkerPublicKey) ||
+            !linkerPublicKey.IsValid)
+        {
+            return false;
+        }
+
+        if (!this.ProofB.Validate() ||
+            !this.ProofB.TryGetLinkerPublicKey(out linkerPublicKey) ||
+            !linkerPublicKey.IsValid)
+        {
+            return false;
+        }
+
+        return true;
+    }
 }
 
 /// <summary>
