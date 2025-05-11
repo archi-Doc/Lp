7affa7feadbe76ce9e6dc2106b419955d90d196f
archi-Doc <archidoc422@gmail.com>
2025-05-11 15:53:20 +0900

    .

--- Lp/T3cs/Base/Linkage.cs
+++ Lp/T3cs/Base/Linkage.cs
@@ -2,12 +2,48 @@
 
 namespace Lp.T3cs;
 
+[TinyhandObject]
+public sealed partial class Linkage<TProof>
+    where TProof : Proof
+{
+    private Linkage()
+    {
+    }
+
+    [Key(0)]
+    public Proof ProofA { get; set; } = default!;
+
+    [Key(1)]
+    public Proof ProofB { get; set; } = default!;
+
+    [Key(2)]
+    private byte[]? mergerSignatureA1;
+
+    [Key(3)]
+    private byte[]? mergerSignatureA2;
+
+    [Key(4)]
+    private byte[]? mergerSignatureA3;
+
+    [Key(5)]
+    private byte[]? mergerSignatureB1;
+
+    [Key(6)]
+    private byte[]? mergerSignatureB2;
+
+    [Key(7)]
+    private byte[]? mergerSignatureB3;
+
+    [Key(8)]
+    private byte[]? linkerSignature;
+}
+
 /// <summary>
 /// Immutable linkage object (authentication between mergers).
 /// </summary>
 [TinyhandObject]
 [ValueLinkObject(Isolation = IsolationLevel.Serializable)]
-public sealed partial class Linkage : IValidatable
+public partial class Linkage : IValidatable
 {// Linkage x Point
     // [Link(Primary = true, TargetMember = "ProofMics", Type = ChainType.Ordered)]
     public Linkage(Evidence linkedEvidence)
