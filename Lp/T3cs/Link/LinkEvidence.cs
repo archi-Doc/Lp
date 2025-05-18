60beac55434e6ec4a9c0541222e9c9805e2a0a46
archi-Doc <archidoc422@gmail.com>
2025-05-18 22:16:21 +0900

    .

---
+++ Lp/T3cs/Link/LinkEvidence.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+namespace Lp.T3cs;
+
+[TinyhandObject]
+// [ValueLinkObject(Integrality = true, Isolation = IsolationLevel.Serializable)]
+public partial class LinkEvidence : Evidence
+{
+    [Key(Evidence.ReservedKeyCount)]
+    public LinkProof LinkProof { get; private set; }
+
+    public override Proof Proof => this.LinkProof;
+
+    public LinkEvidence(LinkProof linkProof)
+    {
+        this.LinkProof = linkProof;
+    }
+}
