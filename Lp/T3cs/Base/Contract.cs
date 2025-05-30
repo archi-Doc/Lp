e339d75247397b30dd62d4cd9d20c3f119e714fd
archi-Doc <archidoc422@gmail.com>
2025-05-30 23:10:43 +0900

    ->

---
+++ Lp/T3cs/Base/Contract.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+using System.Diagnostics.CodeAnalysis;
+using Netsphere.Crypto;
+
+namespace Lp.T3cs;
+
+[TinyhandObject]
+public readonly partial struct Contract : IEquatable<Contract>
+{
+    [Key(0)]
+    public Proof Proof { get; init; }
+
+    [Key(1)]
+    public Point Partial { get; init; }
+
+    [Key(2)]
+    public Point Total { get; init; }
+
+    public Contract(LinkableProof proof, Point partial, Point total)
+    {
+        this.Proof = proof;
+        this.Partial = partial;
+        this.Total = total;
+    }
+
+    public Contract(LinkableProof proof)
+    {
+        this.Proof = proof;
+    }
+
+    public bool Equals(Contract other)
+        => this.Proof.Equals(other.Proof) && this.Partial == other.Partial && this.Total == other.Total;
+}
+
+/*[ValueLinkObject]
+public partial class ContractedLinkage : Linkage
+{
+    public static bool TryCreate(LinkableEvidence evidence1, LinkableEvidence evidence2, [MaybeNullWhen(false)] out MarketableLinkage linkage)
+        => TryCreate(() => new(), evidence1, evidence2, out linkage);
+
+    [Link(Primary = true, Unique = true, Type = ChainType.Ordered, TargetMember = nameof(LinkedMics))]
+    protected ContractedLinkage()
+    {
+    }
+
+    public MarketableProof LinkableProof1 => (MarketableProof)this.BaseProof1;
+
+    public MarketableProof LinkableProof2 => (MarketableProof)this.BaseProof2;
+}*/
---
+++ Lp/T3cs/Base/LinkableEvidence2.cs
@@ -0,0 +1,81 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+using Arc.Collections;
+using Netsphere.Crypto;
+using Tinyhand.IO;
+
+namespace Lp.T3cs;
+
+[TinyhandObject(ReservedKeyCount = LinkableEvidence.ReservedKeyCount)]
+public sealed partial class LinkableEvidence2 : Evidence
+{
+    /// <summary>
+    /// The number of reserved keys.
+    /// </summary>
+    public new const int ReservedKeyCount = Evidence.ReservedKeyCount + 4;
+
+    public static readonly ObjectPool<LinkableEvidence2> Pool = new(() => LinkableEvidence2.UnsafeConstructor());
+
+    #region FieldAndProperty
+
+    [Key(Evidence.ReservedKeyCount + 0, Level = TinyhandWriter.DefaultLevel)]
+    public bool IsPrimary { get; set; }
+
+    [Key(Evidence.ReservedKeyCount + 1)]
+    public long LinkedMicsId { get; private set; }
+
+    [Key(Evidence.ReservedKeyCount + 2)]
+    public Contract Contract1 { get; private set; }
+
+    [Key(Evidence.ReservedKeyCount + 3)]
+    public Contract Contract2 { get; private set; }
+
+    public override Proof BaseProof => this.IsPrimary ? this.Contract1.Proof : this.Contract2.Proof;
+
+    public LinkableProof LinkageProof1 => (LinkableProof)this.Contract1.Proof;
+
+    public LinkableProof LinkageProof2 => (LinkableProof)this.Contract2.Proof;
+
+    #endregion
+
+    public LinkableEvidence2(bool isPrimary, long linkedMicsId, Contract contract1, Contract contract2)
+    {
+        this.IsPrimary = isPrimary;
+        this.LinkedMicsId = linkedMicsId;
+        this.Contract1 = contract1;
+        this.Contract2 = contract2;
+    }
+
+    public (Proof? Proof, int MergerIndex) GetMergerIndex(ref SignaturePublicKey publicKey)
+    {
+        if (this.BaseProof.TryGetCredit(out var credit))
+        {
+            var mergerIndex = credit.GetMergerIndex(ref publicKey);
+            if (mergerIndex >= 0)
+            {
+                return (this.BaseProof, mergerIndex);
+            }
+        }
+
+        return default;
+    }
+
+    internal void FromLinkage(Linkage linkage, bool isPrimary)
+    {
+        this.LinkedMicsId = linkage.LinkedMics;
+        this.Contract1 = new((LinkableProof)linkage.BaseProof1);//
+        this.Contract2 = new((LinkableProof)linkage.BaseProof2);//
+        if (isPrimary)
+        {
+            this.MergerSignature0 = linkage.MergerSignature10;
+            this.MergerSignature1 = linkage.MergerSignature11;
+            this.MergerSignature2 = linkage.MergerSignature12;
+        }
+        else
+        {
+            this.MergerSignature0 = linkage.MergerSignature20;
+            this.MergerSignature1 = linkage.MergerSignature21;
+            this.MergerSignature2 = linkage.MergerSignature22;
+        }
+    }
+}
