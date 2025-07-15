7806e2dbf3ab29135824288807cf087b12d4a4d5
archi-Doc <archidoc422@gmail.com>
2025-07-16 00:40:06 +0900

    ...

--- Lp/T3cs/Credit/FullCredit.cs
+++ Lp/T3cs/Credit/FullCredit.cs
@@ -6,28 +6,59 @@ namespace Lp.T3cs;
 
 #pragma warning disable SA1401
 
+/// <summary>
+/// Represents a full credit entity, including its credit, information, and associated owners.<br/>
+/// This class needs to be thread-safe.
+/// </summary>
 [TinyhandObject(Structual = true)]
 [ValueLinkObject(Isolation = IsolationLevel.RepeatableRead)]
 public partial record FullCredit
 {
     #region FieldAndProperty
 
+    /// <summary>
+    /// Gets the credit associated with this full credit.<br/>
+    /// Thread-safety: Immutable.
+    /// </summary>
     [Link(Primary = true, Unique = true, Type = ChainType.Unordered)]
     [Key(0)]
-    public Credit Credit { get; protected set; } = Credit.UnsafeConstructor();
+    public Credit Credit { get; private set; }
 
+    /// <summary>
+    /// Gets the credit information for this full credit.
+    /// Thread-safety: Immutable instance.
+    /// </summary>
     [Key(1)]
-    public CreditInformation CreditInformation { get; protected set; } = CreditInformation.UnsafeConstructor();
+    public CreditInformation CreditInformation { get; private set; } = CreditInformation.UnsafeConstructor();
 
+    /// <summary>
+    /// Gets the storage data containing owner data for this full credit.
+    /// Thread-safety: Immutable instance.
+    /// </summary>
     [Key(2)]
-    public StorageData<OwnerData.GoshujinClass> Owners { get; protected set; } = new();
+    public StorageData<OwnerData.GoshujinClass> Owners { get; private set; } = new();
 
     #endregion
 
-    public FullCredit()
+    public FullCredit(Credit credit)
     {
+        this.Credit = credit;
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="FullCredit"/> class.<br/>
+    /// This function was added for ValueLink, but I plan to improve the code in the future.
+    /// </summary>
+    private FullCredit()
+    {
+        this.Credit = default!;
+    }
+
+    /// <summary>
+    /// Determines whether the specified <see cref="EvolProof"/> is contained within the owner data of this full credit.
+    /// </summary>
+    /// <param name="proof">The <see cref="EvolProof"/> to check for existence.</param>
+    /// <returns><c>true</c> if the proof exists; otherwise, <c>false</c>.</returns>
     public bool Contains(EvolProof proof)
     {
         var ownerData = this.GetOwnerData(proof.SourceValue.Owner);
@@ -49,6 +80,11 @@ public partial record FullCredit
         return false;
     }
 
+    /// <summary>
+    /// Gets the <see cref="OwnerData"/> for the specified owner public key.
+    /// </summary>
+    /// <param name="ownerPublicKey">The public key of the owner.</param>
+    /// <returns>The <see cref="OwnerData"/> if found; otherwise, <c>null</c>.</returns>
     private OwnerData? GetOwnerData(SignaturePublicKey ownerPublicKey)
     {
         return this.Owners.Get().Result.TryGet(ownerPublicKey);
