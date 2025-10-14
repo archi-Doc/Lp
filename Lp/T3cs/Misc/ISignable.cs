0ff3624eaa5509e2c84ae394682f24daef6e0cfa
archi-Doc <archidoc422@gmail.com>
2025-10-14 21:08:21 +0900

    .

--- Lp/T3cs/Misc/ISignable.cs
+++ Lp/T3cs/Misc/ISignable.cs
@@ -1,25 +1,8 @@
 ﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
 
-using System.Diagnostics.CodeAnalysis;
 using Netsphere.Crypto;
 
 namespace Lp.T3cs;
-
-public readonly struct SignaturePair
-{
-    public readonly int Index;
-
-    public readonly byte[]? Signature;
-
-    public SignaturePair(int index, byte[]? signature)
-    {
-        this.Index = index;
-        this.Signature = signature;
-    }
-
-    [MemberNotNullWhen(true, nameof(Signature))]
-    public bool IsValid => this.Signature is not null;
-}
 
 public interface ISignable
 {
