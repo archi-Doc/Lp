687e53045b51cc783b8151502fa21a77afed5bb7
archi-Doc <archidoc422@gmail.com>
2026-01-20 15:04:48 +0900

    .

---
+++ Lp/Merger/NetServices/IDomainRoot.cs
@@ -0,0 +1,64 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+using Lp.T3cs;
+using Netsphere.Crypto;
+
+namespace Lp.T3cs;
+
+[NetServiceInterface]
+public partial interface IDomainRoot : INetService
+{
+    Task<NetResultAndValue<ConnectionAgreement?>> Authenticate(AuthenticationToken token);
+
+    Task<MergedEvidence?> RequestMergedEvidence();
+
+    Task<PeerProof?> ExchangeCertificateProof(PeerProof peerProof);
+}
+
+/*[NetServiceObject]
+internal class MergerRemoteAgent : IDomainRoot
+{
+    private readonly LpBase lpBase;
+    private readonly Merger merger;
+    private bool authenticated;
+
+    public MergerRemoteAgent(LpBase lpBase, Merger merger)
+    {
+        this.lpBase = lpBase;
+        this.merger = merger;
+    }
+
+    async Task<NetResultAndValue<ConnectionAgreement?>> IMergerRemote.Authenticate(AuthenticationToken token)
+    {
+        var serverConnection = TransmissionContext.Current.ServerConnection;
+        if (!token.ValidateAndVerify(serverConnection) ||
+            !token.PublicKey.Equals(this.lpBase.RemotePublicKey))
+        {
+            return new(NetResult.NotAuthenticated);
+        }
+
+        this.authenticated = true;
+
+        return new((ConnectionAgreement?)default);
+    }
+
+    Task<T3csResult> IMergerRemote.CreateCredit(CreditIdentity creditIdentity)
+    {
+        if (!this.authenticated)
+        {
+            return Task.FromResult(T3csResult.NotAuthenticated);
+        }
+
+        return this.merger.CreateCredit(creditIdentity);
+    }
+
+    async Task<SignaturePublicKey> IMergerRemote.GetMergerKey()
+    {
+        if (!this.authenticated)
+        {
+            return default;
+        }
+
+        return this.merger.PublicKey;
+    }
+}*/
