72af01e368475674aa6372088377bbc43b65efb5
archi-Doc <archidoc422@gmail.com>
2026-02-17 23:03:16 +0900

    .

---
+++ Lp/T3cs/Base/Proof/CertificateProof.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+using Netsphere.Crypto;
+
+namespace Lp.T3cs;
+
+[TinyhandObject(AddAlternateKey = true)]
+public partial class CertificateProof : ProofWithPublicKey
+{
+    #region FieldAndProperty
+
+    [Key(ProofWithPublicKey.ReservedKeyCount)]
+    public MergedProof MergedProof { get; protected set; } = new();
+
+    [Key(ProofWithPublicKey.ReservedKeyCount + 1)]
+    public NetNode NetNode { get; protected set; } = new();
+
+    #endregion
+
+    public CertificateProof(SignaturePublicKey publicKey)
+        : base(publicKey)
+    {
+    }
+
+    public override bool Validate(ValidationOptions validationOptions)
+    {
+        if (!base.Validate(validationOptions))
+        {
+            return false;
+        }
+
+        if (!this.NetNode.Validate())
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
